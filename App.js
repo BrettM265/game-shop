8cc53caa750ee1422e4f0ea456be4ace86116a0b
BrettM265 <78045071+BrettM265@users.noreply.github.com>
2022-04-10 19:36:09 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Products from './products';
-import FilterPrice from './Sidebar-filter-price';
+import Cart from './cart';
 import Games from './Games';
 
 function App() {
+  
   return (
     <>
     <div className="store-home">
+    <Cart />
       <div className="store-head">
       <h1 className='store-heading'>Bretts Bin of Classics</h1>
       <h3>Ayyy Ooooo Capernicus<br></br>With the Big Shoes and Fancy Car<br />Come down to Chi Town and checkout my Games!</h3>
