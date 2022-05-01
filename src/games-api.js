8f636e331380a9bde707506496fa3664a0128e4b
BrettM265 <BDMgit@gmail.com>
2022-05-01 16:40:43 -0500

    image changes

--- src/games-api.js
+++ src/games-api.js
@@ -3,119 +3,119 @@ const gameList = [{
     title:"WoW",
     searchterm:"World of Warcraft WoW",
     price: 15.00,
-    image:"./game-shop/imgs/WoW.jpg",
+    image:".imgs/WoW.jpg",
     console:"PC"
 },{
     id:1,
     title:"BattleField BC2",
     searchterm:"BattleField BC2 Bad Company",
     price: 2.00,
-    image:"./game-shop/imgs/BFBC2.jpg",
+    image:".imgs/BFBC2.jpg",
     console:"Xbox360"
 },{
     id:2,
     title:"Dark Souls",
     searchterm:"Dark Souls DS1",
     price: 15.00,
-    image:"./game-shop/imgs/DS1.jpg",
+    image:".imgs/DS1.jpg",
     console:"PS3 XboxOne PC PS4 Xbox360"
 },{
     id:3,
     title:"Dark Souls 2",
     searchterm:"Dark Souls 2 DS2",
     price: 20.00,
-    image:"./game-shop/imgs/DS2.jpg",
+    image:".imgs/DS2.jpg",
     console:"PS3 XboxOne PC PS4 Xbox360"
 },{
     id:4,
     title:"Dark Souls 3",
     searchterm:"Dark Souls 3 DS3",
     price: 15.00,
-    image:"./game-shop/imgs/DS3.jpg",
+    image:".imgs/DS3.jpg",
     console:"XboxOne PC PS4"
 },{
     id:5,
     title:"Demons Souls",
     searchterm:"Demons Souls DS",
     price: 15.00,
-    image:"./game-shop/imgs/DS.jpg",
+    image:".imgs/DS.jpg",
     console:"PS3"
 },{
     id:6,
     title:"Dragon Age IQ",
     searchterm:"DAI Dragon Age Inquisition",
     price: 10.00,
-    image:"./game-shop/imgs/DAI.jpg",
+    image:".imgs/DAI.jpg",
     console:"PS3 XboxOne PC PS4 Xbox360"
 },{
     id:7,
     title:"CupHead",
     searchterm:"Cuphead",
     price: 20.00,
-    image:"./game-shop/imgs/CH.jpg",
+    image:".imgs/CH.jpg",
     console:"XboxOne PC PS4"
 },{
     id:8,
     title:"COD WaW",
     searchterm:"Cod Waw Call of duty world at war",
     price: 3.00,
-    image:"./game-shop/imgs/CODWAW.png",
+    image:".imgs/CODWAW.png",
     console:"PS3 PC Xbox360"
 },{
     id:9,
     title:"ES Oblivion",
     searchterm:"ESO Elder Scrolls Oblivion",
     price: 20.00,
-    image:"./game-shop/imgs/ESOB.jpg",
+    image:".imgs/ESOB.jpg",
     console:"PS3 Xbox360 PC"
 },{
     id:10,
     title:"Gears Of War",
     searchterm:"GOW Gears of war",
     price: 1.00,
-    image:"./game-shop/imgs/GOW.jpg",
+    image:".imgs/GOW.jpg",
     console:"PS3 Xbox360"
 },{
     id:11,
     title:"Halo 2",
     searchterm:"Halo 2",
     price: 10.00,
-    image:"./game-shop/imgs/Halo 2.png",
+    image:".imgs/Halo 2.png",
     console:"Xbox PC"
 },{
     id:12,
     title:"Halo 3",
     searchterm:"Halo 3",
     price: 20.00,
-    image:"./game-shop/imgs/Halo 3.jpg",
+    image:".imgs/Halo 3.jpg",
     console:"Xbox One Xbox360 PC"
 },{
     id:13,
     title:"MK11",
     searchterm:"MK11 Mortal Kombat 11",
     price: 7.00,
-    image:"./game-shop/imgs/MK11.png",
+    image:".imgs/MK11.png",
     console:"PS4 XboxOne PC"
 },{
     id:14,
     title:"NBA Jam",
     searchterm:"Nba Jam",
     price: 10.00,
-    image:"./game-shop/imgs/NBA Jam.jpg",
+    image:".imgs/NBA Jam.jpg",
     console:"PS3 Xbox360 PC PS4 Xbox One"
 },{
     id:15,
     title:"Skyrim",
     searchterm:"Skyrim Elder Scrolls ES",
     price: 30.00,
-    image:"./game-shop/imgs/Skyrim.png",
+    image:".imgs/Skyrim.png",
     console:"PS3 Xbox360 PC PS4 XboxOne"
 },{
     id:16,
     title:"Titanfall 2",
     searchterm:"TF2 Titanfall 2",
     price: 16.00,
-    image:"./game-shop/imgs/TF2.jpg",
+    image:".imgs/TF2.jpg",
     console:"XboxOne PS4 PC"
 }]
 
